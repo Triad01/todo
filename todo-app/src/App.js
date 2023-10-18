c98c9dd2be1990486644ff856d6e08f1453d4c60
Triad01 <luckypee01@gmail.com>
2023-10-18 04:46:30 +0100

    clean up files

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,7 +1,11 @@
+import Test from "./components/test";
+
 function App() {
   return (
     <div className="App">
       <h1 className="text-red-500 text-2xl text-center bg-yellow-500">Hello</h1>
+      <p className="bg-green-500 text-center">This is a dummy paragraph</p>
+      <Test />
     </div>
   );
 }
---
+++ todo-app/src/components/test.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Test = () => {
+  return (
+    <div className="text-center bg-slate-600 text-xl">
+      <h1 className="text-violet-500">This is from a child component</h1>
+      <p className="bg-red-400 text-white">
+        This is just for test purpose, keep it up!
+      </p>
+    </div>
+  );
+};
+
+export default Test;
